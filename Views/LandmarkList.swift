2f6bc4908e750b4080b65f816d963b38bfcd6f5f
Luslec <91324720+JEONJEYUN@users.noreply.github.com>
2023-03-08 00:43:15 +0900

    목록 및 탐색 구축 2

--- Views/LandmarkList.swift
+++ Views/LandmarkList.swift
@@ -12,7 +12,7 @@ struct LandmarkList: View {
         NavigationView {
             List(landmarks) { landmark in
                 NavigationLink {
-                    LandmarkDetail()
+                    LandmarkDetail(landmark: landmark)
                 } label: {
                     LandmarkRow(landmark: landmark)
                 }
@@ -25,5 +25,10 @@ struct LandmarkList: View {
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkList()
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
