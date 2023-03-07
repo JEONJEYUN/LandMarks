3a5ab0554ad74989a16abc0ce0ecf795252f0d50
Luslec <91324720+JEONJEYUN@users.noreply.github.com>
2023-03-07 22:57:51 +0900

    목록 및 탐색 구축 1

---
+++ Views/LandmarkList.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by 전제윤 on 2023/03/07.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail()
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
